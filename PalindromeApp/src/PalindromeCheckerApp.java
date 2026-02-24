583f99527207e4a51403770554ddee0d29e0abe5
rehnazraffiq <rm4109@srmist.edu.in>
2026-02-24 09:41:49 +0530

    [rehnazrafffiq] Added UC2: Print a Hardcoded Palindrome Result

--- PalindromeApp/src/PalindromeCheckerApp.java
+++ PalindromeApp/src/PalindromeCheckerApp.java
@@ -1,7 +1,17 @@
 public class PalindromeCheckerApp {
+    public static boolean isPalindrome(String str){
+        int length = str.length();
+        for (int i = 0;i < length/2;i++){
+            if (str.charAt(i)!=str.charAt(length-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
     public static void main(String[] args){
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully.");
+        String str = "madam";
+        boolean result = isPalindrome(str);
+        System.out.println("Input text: " +str);
+        System.out.println("Is it a Palindrome?: " +result);
     }
 }
